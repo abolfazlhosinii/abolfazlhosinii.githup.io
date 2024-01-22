ec5910f11ebd0659f6886d88abc0d1facb25138e
abolfazl <abolfazl123456kk@gmail.com>
2024-01-22 19:30:05 +0330

    carousel
    Making a carousel and photos

--- Main/js/hi abolfal.js
+++ Main/js/hi abolfal.js
@@ -16,3 +16,24 @@ anime.timeline({ loop: true })
         easing: "easeOutExpo",
         delay: 1000
     });
+
+
+//img 
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml3 .letter',
+        opacity: [0, 1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i + 1)
+    }).add({
+        targets: '.ml3',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
